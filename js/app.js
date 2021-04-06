7ee49b97e03c9037dc86f35e4c287a95b47763c4
Angélique Le Roux <angelique.leroux_stg@cooperl.com>
2021-04-06 16:24:56 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -1,55 +1,23 @@
 const container = document.querySelector(".container");
-const coffees = [
-  {
-    name: "Perspiciatis",
-    image: "images/coffee1.jpg"
-  },
-  {
-    name: "Voluptatem",
-    image: "images/coffee2.jpg"
-  },
-  {
-    name: "Explicabo",
-    image: "images/coffee3.jpg"
-  },
-  {
-    name: "Rchitecto",
-    image: "images/coffee4.jpg"
-  },
-  {
-    name: " Beatae",
-    image: "images/coffee5.jpg"
-  },
-  {
-    name: " Vitae",
-    image: "images/coffee6.jpg"
-  },
-  {
-    name: "Inventore",
-    image: "images/coffee7.jpg"
-  },
-  {
-    name: "Veritatis",
-    image: "images/coffee8.jpg"
-  },
-  {
-    name: "Accusantium",
-    image: "images/coffee9.jpg"
-  }
-];
+
 const showCoffees = () => {
   let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
-  container.innerHTML = output;
+
+  fetch("https://dev-coffee-server.herokuapp.com/api/coffee")
+    .then(response => response.json())
+    .then(coffees => {
+      coffees.forEach(
+        ({ name, image }) =>
+          (output += `
+                  <div class="card">
+                    <img class="card--avatar" src=${image} alt="image ${name}"/>
+                    <h1 class="card--title">${name}</h1>
+                    <a class="card--link" href="#">Taste</a>
+                  </div>
+                  `)
+      );
+      container.innerHTML = output;
+    })
 };
 
 document.addEventListener("DOMContentLoaded", showCoffees);
@@ -57,5 +25,8 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     /** Ici nous allons enregistrer notre service worker */
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then(() => console.log('Service worker enregistré avec succès'))
+      .catch((err) => console.error("Le service worker n'a pas été enregistré", err))
   });
 }
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,34 @@
 /**
  *  Ici nous allons ecouter l'évenement install et nous ferons un console log
  */
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open('dev-coffee-v1')
+      .then((cache) => {
+        cache.addAll([
+          '/', '/index.html', '/css/style.css', '/js/app.js'
+        ])
+      })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  if(event.request.url.indexOf('http') === 0) {
+    event.respondWith(
+      caches.match(event.request)
+        .then((cacheRessources) => {
+          if(cacheRessources) {
+            return cacheRessources
+          }
+
+          return fetch(event.request)
+            .then((response) => {
+              const responseToCache = response.clone()
+              caches.open('dev-coffee-v1').then(cache => {
+                cache.put(event.request, responseToCache)
+              })
+            })
+        })
+    )
+  }
+})
